ab7095e993339336e9a80c9e15d8d8711864e31f
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-08-23 18:19:28 -0500

    Project creation

---
+++ src/test/java/com/bootcamp/microserviceStock/MicroserviceStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.microserviceStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
