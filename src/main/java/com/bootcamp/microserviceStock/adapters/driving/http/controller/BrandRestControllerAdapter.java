72b25f4a826b88e903235cdb36288f359504df38
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-08-29 21:06:02 -0500

    Corrected validations handling in BrandRequest HU3

--- src/main/java/com/bootcamp/microserviceStock/adapters/driving/http/controller/BrandRestControllerAdapter.java
+++ src/main/java/com/bootcamp/microserviceStock/adapters/driving/http/controller/BrandRestControllerAdapter.java
@@ -2,8 +2,9 @@ package com.bootcamp.microserviceStock.adapters.driving.http.controller;
 
 import com.bootcamp.microserviceStock.adapters.driving.http.dto.request.BrandRequest;
 import com.bootcamp.microserviceStock.adapters.driving.http.mapper.IBrandRequestMapper;
-import com.bootcamp.microserviceStock.adapters.driving.http.util.DrivingConstants;
 import com.bootcamp.microserviceStock.domain.api.IBrandServicePort;
+import com.bootcamp.microserviceStock.domain.util.DomainConstants;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,8 +23,8 @@ public class BrandRestControllerAdapter {
     private final IBrandRequestMapper brandRequestMapper;
 
     @PostMapping("/create")
-    public ResponseEntity<ControllerResponse> createBrand(@RequestBody BrandRequest request) {
+    public ResponseEntity<ControllerResponse> createBrand(@Valid @RequestBody BrandRequest request) {
         brandServicePort.createBrand(brandRequestMapper.requestToBrand(request));
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ControllerResponse(DrivingConstants.BRAND_CREATED_MESSAGE, HttpStatus.CREATED.toString(), LocalDateTime.now()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ControllerResponse(DomainConstants.BRAND_CREATED_MESSAGE, HttpStatus.CREATED.toString(), LocalDateTime.now()));
     }
 }
