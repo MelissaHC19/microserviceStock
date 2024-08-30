ad9b25e1e5e8e599fc499d1f3d1ea8b166b5ee4a
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-08-29 20:30:40 -0500

    Fixed DomainConstants

--- src/main/java/com/bootcamp/microserviceStock/domain/util/DomainConstants.java
+++ src/main/java/com/bootcamp/microserviceStock/domain/util/DomainConstants.java
@@ -11,4 +11,5 @@ public final class DomainConstants {
     public static final int MAX_FIELD_SIZE_DESCRIPTION = 90;
     public static final String MAX_FIELD_SIZE_NAME_MESSAGE = "Field 'name' exceeds the maximum allowed character limit of 50 characters.";
     public static final String MAX_FIELD_SIZE_DESCRIPTION_MESSAGE = "Field 'description' exceeds the maximum allowed character limit of 90 characters.";
+    public static final String CATEGORY_CREATED_MESSAGE = "Category created successfully.";
 }
