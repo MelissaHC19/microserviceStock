55af1e1bb84587d9ee6ba7a0bec5a1ed0d83e93d
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-09-08 13:55:02 -0500

    Open API Documentation

--- src/main/java/com/bootcamp/microserviceStock/adapters/driving/http/controller/ArticleRestControllerAdapter.java
+++ src/main/java/com/bootcamp/microserviceStock/adapters/driving/http/controller/ArticleRestControllerAdapter.java
@@ -2,8 +2,14 @@ package com.bootcamp.microserviceStock.adapters.driving.http.controller;
 
 import com.bootcamp.microserviceStock.adapters.driving.http.dto.request.ArticleRequest;
 import com.bootcamp.microserviceStock.adapters.driving.http.mapper.IArticleRequestMapper;
+import com.bootcamp.microserviceStock.configuration.exceptionHandler.ExceptionResponse;
 import com.bootcamp.microserviceStock.domain.api.IArticleServicePort;
 import com.bootcamp.microserviceStock.domain.util.DomainConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,6 +28,19 @@ public class ArticleRestControllerAdapter {
     private final IArticleServicePort articleServicePort;
     private final IArticleRequestMapper articleRequestMapper;
 
+    @Operation(summary = "Create article",
+            tags = {"Article"},
+            description = "This endpoint allows the creation of a new article. The article name and description must " +
+                    "follow specific length requirements and cannot be empty, and an article with the same name must not " +
+                    "already exist. If any of these conditions are violated, the request will result in an error response " +
+                    "detailing the validation issues."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Article created successfully",
+                    content = @Content(schema = @Schema(implementation = ControllerResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Article not created",
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
+    })
     @PostMapping("/create")
     public ResponseEntity<ControllerResponse> createArticle(@Valid @RequestBody ArticleRequest request) {
         articleServicePort.createArticle(articleRequestMapper.requestToArticle(request));
