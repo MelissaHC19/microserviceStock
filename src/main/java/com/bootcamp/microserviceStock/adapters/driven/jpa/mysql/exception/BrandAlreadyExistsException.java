405526002e1fb3da14b44ea602fbecc7526cb479
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-09-08 14:15:42 -0500

    Deleted unused exceptions

--- src/main/java/com/bootcamp/microserviceStock/adapters/driven/jpa/mysql/exception/BrandAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.microserviceStock.adapters.driven.jpa.mysql.exception;
-
-public class BrandAlreadyExistsException extends RuntimeException {
-    public BrandAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/bootcamp/microserviceStock/adapters/driven/jpa/mysql/exception/CategoryAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.microserviceStock.adapters.driven.jpa.mysql.exception;
-
-public class CategoryAlreadyExistsException extends RuntimeException {
-    public CategoryAlreadyExistsException(String message) {
-        super(message);
-    }
-}
