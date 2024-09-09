731588b2ecf4804e25d077539d5b576774149e7c
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-09-08 21:24:25 -0500

    Corrected IArticleEntityMapper

--- src/main/java/com/bootcamp/microserviceStock/adapters/driven/jpa/mysql/mapper/IArticleEntityMapper.java
+++ src/main/java/com/bootcamp/microserviceStock/adapters/driven/jpa/mysql/mapper/IArticleEntityMapper.java
@@ -3,9 +3,13 @@ package com.bootcamp.microserviceStock.adapters.driven.jpa.mysql.mapper;
 import com.bootcamp.microserviceStock.adapters.driven.jpa.mysql.entity.ArticleEntity;
 import com.bootcamp.microserviceStock.domain.model.Article;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IArticleEntityMapper {
+    @Mapping(target = "categoryEntityList", source = "categoryList")
     ArticleEntity articleToEntity(Article article);
+
+    @Mapping(target = "categoryList", source = "categoryEntityList")
     Article entityToDomain(ArticleEntity articleEntity);
 }
